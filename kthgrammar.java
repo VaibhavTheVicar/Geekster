c4c635845e48439b2ff4c4ec0675ae0b6d898371
VaibhavTheVicar <Vaibhavtiwari.ece17@nituk.ac.in>
2021-03-13 18:24:36 +0530

    kth grammer

--- kthgrammar.java
+++ kthgrammar.java
@@ -1,5 +1,20 @@
 public class kthgrammar {
     public static void main(String[] args) {
-        System.out.println("jello");
+        int n = 4;
+        int k = 5;
+        System.out.println(kthGrammar(n,k));
+    }
+    public static int kthGrammar(int N, int K) {
+        if(N==1 && K==1){
+            return 0;
+        }
+        int ans;
+        if(K%2==0)
+            ans =  1-kthGrammar(N-1,K/2);
+        else
+            ans = kthGrammar(N-1,(K+1)/2);
+        return ans;
+
+
     }
 }
