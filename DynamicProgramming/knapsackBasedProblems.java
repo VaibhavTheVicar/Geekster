f90534de7a944366a87742260fb96e5113062b35
VaibhavTheVicar <Vaibhavtiwari.ece17@nituk.ac.in>
2021-04-01 21:46:27 +0530

    update

--- DynamicProgramming/knapsackBasedProblems.java
+++ DynamicProgramming/knapsackBasedProblems.java
@@ -1,7 +1,9 @@
 package DynamicProgramming;
-
+import java.util.*;
 public class knapsackBasedProblems {
     public static void main(String[] args) {
+        // int j = new Scanner(System.in).nextInt();
+        // System.out.println(j);
 		minimumDifferenceBetweenSubsetSum(new int[]{ 68, 35, 1, 70, 25, 79, 59, 63, 65, 6, 46, 82, 28, 62, 92, 96, 43, 28, 37, 92, 5, 3, 54, 93, 83, 22, 17, 19, 96, 48, 27, 72, 39, 70, 13, 68, 100, 36, 95, 4, 12, 23, 34, 74, 65, 42, 12, 54, 69, 48, 45});
 	}
 
