d2dc89a05e6b0767abec66d4eb842b9d3e75ecff
VaibhavTheVicar <Vaibhavtiwari.ece17@nituk.ac.in>
2021-04-03 14:02:03 +0530

    sublime

--- Practice1/Solution.java
+++ Practice1/Solution.java
@@ -3,36 +3,37 @@ package Practice1;
 import java.util.*;
 public class Solution {
     public static void main(String[] args) {
-        int arr[] = {1,3,2,0,0,4};
+        int arr[] = {1,3,0,0,2,4};
         int n = arr.length;
-        int [] ans= NextGreaterElement(arr,n);
-    }
-
-    static int[] NextGreaterElement(int arr[], int n){
-
-        Stack<Integer> st = new Stack<>(); 
-        //List<Integer> l = new ArrayList<>();
         int ans[] = new int[n];
-        for(int i = n-1;i>=0;i--){
-            int ele= arr[i];
-            if(st.empty())
-                ans[i] = -1;
-            else if(!st.empty() && st.peek()>ele)
-                ans[i] = st.peek();
-
-            else if(!st.empty() && st.peek()<=arr[i])
-                {
-                    while(!st.empty() && st.peek()<=arr[i])
-                        st.pop();
-                    if(st.empty())
-                        ans[i] = -1;
-                    else if(!st.empty() && st.peek()>ele)
-                        ans[i] = st.peek();
-                }
-
-            //l.add(-1);//for n-1 
-
-        }
-        return ans;
+        nge(arr,n,ans);
+        for(int i: ans)
+            System.out.println(i);
+        
     }
+
+    static int[] nge(int arr[],int n,int[] ans){
+        //For every element in an array
+        Stack<Integer> st =new Stack<>();
+        for (int i = n-1; i >= 0; i--) {
+            //St.empty() -> ans=-1
+            if(st.empty()) ans[i] = -1;//#1
+
+            else if(st.peek()>arr[i]) ans[i] = st.peek();//#2
+
+            else if(st.peek()<=arr[i]){
+                while(!st.empty() && st.peek()<=arr[i]) st.pop();
+                if(st.empty()) ans[i] = -1;//#1
+
+                else if(st.peek()>arr[i]) ans[i] = st.peek();//#2
+ 
+            }
+            st.push(arr[i]);
+        }
+        
+        
+        return ans;
+
+    }
+
 }
